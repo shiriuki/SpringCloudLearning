7abe796d762d727af64076e45319cb06cdcbb55e
Manuel Calvo <shiriuki@hotmail.com>
2022-12-09 07:04:35 -0600

    Fine tuning gRPC.
    Fine tuning gRPC.

---
+++ exchange-rate/src/main/java/com/mecc/grpcservice/ServerConfiguration.java
@@ -0,0 +1,28 @@
+package com.mecc.grpcservice;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.grpc.netty.NettyServerBuilder;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import net.devh.boot.grpc.server.serverfactory.GrpcServerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+// Fine tunning gRPC see:
+// https://medium.com/@mahdiyusefi72/first-step-to-tune-grpc-in-java-8b57f7f0f591
+
+@Configuration
+public class ServerConfiguration {
+
+    @Bean
+    public GrpcServerConfigurer fineTuneServerConfigurer() {
+        return serverBuilder -> ((NettyServerBuilder) serverBuilder)
+            .bossEventLoopGroup(new NioEventLoopGroup(1))
+            .workerEventLoopGroup(new NioEventLoopGroup(5))
+            .channelType(NioServerSocketChannel.class)
+            .executor(Executors.newFixedThreadPool(5,
+                    new ThreadFactoryBuilder().setNameFormat("grpc-%d").build()));
+    }
+}
