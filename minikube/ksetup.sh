324b681b1087be1e0e924954ea8f91b2c85246b4
Manuel Calvo <shiriuki@hotmail.com>
2023-04-08 08:36:34 -0600

    Need to set local-path a storage provisioner in minikube after each restart.

--- minikube/ksetup.sh
+++ minikube/ksetup.sh
@@ -14,7 +14,11 @@ minikube -p multi start --insecure-registry "192.168.205.0/25" --nodes=4
 minikube -p multi addons enable metrics-server
 
 kubectl config use-context multi
-kubectl label nodes multi-m02 mecc.com/node-type=backend
+#kubectl label nodes multi-m02 mecc.com/node-type=backend
 
 minikube profile docker
 kubectl config use-context multi
+
+k patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+k apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.24/deploy/local-path-storage.yaml
+k patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
--- minikube/ksta.sh
+++ minikube/ksta.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-minikube -p multi start --insecure-registry "192.168.205.0/25" --nodes=4
+minikube -p multi start --insecure-registry "192.168.205.0/25"
 
 kubectl config use-context multi
-kubectl label nodes multi-m02 mecc.com/node-type=backend
 
 minikube profile docker
 kubectl config use-context multi
+
+k patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+k patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
